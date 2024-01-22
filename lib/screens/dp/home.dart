ce24f861620edb33c8d37089c726d5ea8efc171b
ej070961 <ej070961@gmail.com>
2024-01-22 17:03:23 +0900

    feat: 공통위젯 구현(#3)

---
+++ lib/screens/dp/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State <HomePage> createState() =>  HomePageState();
+}
+
+class  HomePageState extends State <HomePage> {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(body: Center(child: Text('home 화면 입니다.')));
+  }
+}
