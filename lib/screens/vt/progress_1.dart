0fcfcb36044bea8f1956a0da47987f04bf4af802
changha <mentenseoul@gmail.com>
2024-01-29 01:08:06 +0900

    feat: 진행상황 UI 완성

--- lib/screens/vt/progress_1.dart
+++ lib/screens/vt/progress_1.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:pathpal/theme.dart';
+import 'package:pathpal/utils/app_images.dart';
+import 'package:pathpal/widgets/build_image.dart';
+import 'package:pathpal/widgets/dp_info.dart';
 import 'package:pathpal/widgets/next_button.dart';
 import 'package:pathpal/widgets/progress_app_bar.dart';
 import 'package:pathpal/widgets/stepper.dart';
 
+import '../../colors.dart';
+
 class VtProgress extends StatefulWidget {
+  String? arriveTime = "";
+
+  VtProgress({this.arriveTime});
+
   @override
   State<VtProgress> createState() => _VtProgressState();
 }
@@ -26,15 +36,78 @@ class _VtProgressState extends State<VtProgress> {
               Container(
                 height: stepperHeight,
                 child: CustomStepper(
-                  steps: ['가는중', '도착 완료'],
+                  steps: ['가는중', '탑승 완료'],
                   currentStep: _currentStep,
                 ),
               ),
+              Expanded(
+                  child: Column(
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("취소");
+                      },
+                      child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          BuildImage.buildImage(AppImages.cancelIconImagePath, width: 13),
+                          Text(
+                            "봉사 취소하기",
+                            style: appTextTheme()
+                                ?.bodyMedium
+                                ?.copyWith(color: Colors.red), // 텍스트 색상 빨강으로 변경
+                          ),
+                        ],
+                      ),
+                      style: ButtonStyle(
+                        overlayColor: MaterialStateProperty.all(Colors.transparent), // 클릭 시 색상 변경 없애기
+                        backgroundColor: MaterialStateProperty.all(Colors.white),
+                        elevation: MaterialStateProperty.all(0), // 그림자 없애기
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero, // 테두리 둥글게 만드는 효과 없애기
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    child: Container(
+                        margin: EdgeInsets.fromLTRB(30.0, 8.0, 0, 0),
+                        child: Text(
+                          '${widget.arriveTime}',
+                          style: appTextTheme().bodyLarge,
+                        ),),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                        bottom: BorderSide(
+                          color: gray200,
+                          width: 0.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                  DpInfo(
+                    backgroundColor: Colors.white,
+                  ),
+                ],
+              )),
               NextButton(
-                title: "도착 완료",
-                onPressed: (){
+                title: "탑승 완료",
+                onPressed: () {
                   setState(() {
-                    if (_currentStep < 1) {
+                    if (_currentStep < 3) {
                       _currentStep++;
                     }
                   });
